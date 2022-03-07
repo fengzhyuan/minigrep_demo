1d5a48d76f172361590013b521e3db459b20e035
Zhongyuan Feng <zfeng@adobe.com>
2022-03-07 10:27:49 -0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::{env, process};
+
+use minigrep::Config;
+use minigrep::run;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        eprintln!("Exception occured: {}", e);
+        process::exit(1);
+    }
+}
+
+
